c910647177a40c822b26e7c28aef373a149c67c2
RUSHABH GALA <88271018+rvgala29@users.noreply.github.com>
2022-09-26 17:03:17 +0530

    Create BinarySearchTreeInsertion.cpp

---
+++ Binary Search Tree : Insertion/BinarySearchTreeInsertion.cpp
@@ -0,0 +1,85 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Node {
+    public:
+        int data;
+        Node *left;
+        Node *right;
+        Node(int d) {
+            data = d;
+            left = NULL;
+            right = NULL;
+        }
+};
+
+class Solution {
+    public:
+  	
+  	void preOrder(Node *root) {
+		
+      	if( root == NULL )
+          	return;
+      
+      	std::cout << root->data << " ";
+      	
+      	preOrder(root->left);
+      	preOrder(root->right);
+    }
+
+/*
+Node is defined as 
+
+class Node {
+    public:
+        int data;
+        Node *left;
+        Node *right;
+        Node(int d) {
+            data = d;
+            left = NULL;
+            right = NULL;
+        }
+};
+
+*/
+    Node * insert(Node * root, int data12) {
+        // If the root is empty
+        // create a new node with specific value
+        if(!root) {
+            root = new Node(data12);
+            return root;
+        }
+        // As this is BST, if data is less than node's value
+        // it will be on left side
+        // else, it will be on right side
+        if(data12<root->data) {
+            root->left = insert(root->left, data12);
+        } else {
+            root->right = insert(root->right, data12);
+        }
+        return root;
+    }
+
+};
+
+int main() {
+  
+    Solution myTree;
+    Node* root = NULL;
+    
+    int t;
+    int data;
+
+    std::cin >> t;
+
+    while(t-- > 0) {
+        std::cin >> data;
+        root = myTree.insert(root, data);
+    }
+  	
+    myTree.preOrder(root);
+  
+    return 0;
+}
