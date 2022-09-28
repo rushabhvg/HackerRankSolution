f782d52449e330d9a4979b81eccc8e6ee76408e1
RUSHABH GALA <88271018+rvgala29@users.noreply.github.com>
2022-09-28 16:32:52 +0530

    Update BalancedBrackets.cpp

--- Balanced Brackets/BalancedBrackets.cpp
+++ Balanced Brackets/BalancedBrackets.cpp
@@ -1,89 +1,51 @@
-#include <bits/stdc++.h>
+#include <stack>
+#include <string>
+#include <iostream>
 
 using namespace std;
 
-string ltrim(const string &);
-string rtrim(const string &);
-
-
-
-
-
-// CODE WRITTEN BY ME
 string isBalanced(string s) {
-    // As it is a stack based question, I am using stack
-    stack<string> stackVar;
-    // By default brackets are balanced
-    string ans="YES";
-    // Iterating over each character of string
-    for (char ch : s) {
-        // Pushing all the opening brackets
-        // And, in case the closing bracket is not matching the topmost bracket
-        // The ans will be no, as the brackets are unbalanced
-        // If closing brackets are matching the top most opening brakets
-        // It is poped from the stack
-        if(ch=='{' or ch=='(' or ch=='[') {
-            stackVar.push(string(1, ch));
-        } else if(ch=='}' and stackVar.top()!="{") {
-            ans="NO";
-        } else if(ch==')' and stackVar.top()!="(") {
-            ans="NO";
-        } else if(ch==']' and stackVar.top()!="[") {
-            ans="NO";
-        } else {
-            stackVar.pop();
+    stack<char> st;  
+    
+    for (auto c: s) {
+        switch (c) {
+            case '{':
+            case '(':
+            case '[':
+                st.push(c);
+                break;
+            case '}':
+                if (st.empty() || (st.top() != '{')) {
+                    return "NO";
+                }
+                st.pop();
+                break;
+            case ')':
+                if (st.empty() || (st.top() != '(')) {
+                    return "NO";
+                }
+                st.pop();
+                break;
+            case ']':
+                if (st.empty() || (st.top() != '[')) {
+                    return "NO";
+                }
+                st.pop();
+                break;
         }
     }
-    return ans;
+    
+    return st.empty() ? "YES" : "NO";
 }
-//END OF CODE
 
 
-
-
-
-
-int main()
-{
-    ofstream fout(getenv("OUTPUT_PATH"));
-
-    string t_temp;
-    getline(cin, t_temp);
-
-    int t = stoi(ltrim(rtrim(t_temp)));
-
-    for (int t_itr = 0; t_itr < t; t_itr++) {
+int main(){
+    int t;
+    cin >> t;
+    for(int a0 = 0; a0 < t; a0++){
         string s;
-        getline(cin, s);
-
-        string result = isBalanced(s);
-
-        fout << result << "\n";
+        cin >> s;
+        cout << isBalanced(s) << endl;
     }
-
-    fout.close();
-
     return 0;
-}
-
-string ltrim(const string &str) {
-    string s(str);
-
-    s.erase(
-        s.begin(),
-        find_if(s.begin(), s.end(), not1(ptr_fun<int, int>(isspace)))
-    );
-
-    return s;
-}
-
-string rtrim(const string &str) {
-    string s(str);
-
-    s.erase(
-        find_if(s.rbegin(), s.rend(), not1(ptr_fun<int, int>(isspace))).base(),
-        s.end()
-    );
-
-    return s;
 }
