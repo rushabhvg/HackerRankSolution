1b8dce5ba27661d42094a8668d192ba6005acc7b
RUSHABH GALA <88271018+rushabhvg@users.noreply.github.com>
2023-08-07 20:27:39 +0530

    Create diagonal_difference.py

---
+++ Diagonal Difference/diagonal_difference.py
@@ -0,0 +1,65 @@
+# /*
+# BY RUSHABH GALA
+# rushabhvg
+# GitHub :
+# https://github.com/rushabhvg
+# StackOverflow :
+# https://stackoverflow.com/users/16571212/rushabhvg
+# LinkedIn:
+# https://www.linkedin.com/in/rushabhvg/
+# */
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'diagonalDifference' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts 2D_INTEGER_ARRAY arr as parameter.
+#
+
+def diagonalDifference(arr):
+    leftItr = 0
+    rightItr = 0
+    n = len(arr)
+
+    for i in range(n):            
+        leftItr += arr[i][i]
+        rightItr += arr[i][n -1]
+        n -= 1
+
+    result = abs(leftItr - rightItr)
+    return result
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    arr = []
+
+    for _ in range(n):
+        arr.append(list(map(int, input().rstrip().split())))
+
+    result = diagonalDifference(arr)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
+
+# /*
+# BY RUSHABH GALA
+# rushabhvg
+# GitHub :
+# https://github.com/rushabhvg
+# StackOverflow :
+# https://stackoverflow.com/users/16571212/rushabhvg
+# LinkedIn:
+# https://www.linkedin.com/in/rushabhvg/
+# */
