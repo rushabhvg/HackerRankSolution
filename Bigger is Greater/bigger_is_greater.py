ee7cda1633b15156d3e1c42b1bf1323afa05ac0e
RUSHABH GALA <88271018+rushabhvg@users.noreply.github.com>
2023-08-09 21:56:35 +0530

    Create bigger_is_greater.py

---
+++ Bigger is Greater/bigger_is_greater.py
@@ -0,0 +1,60 @@
+# /*
+# BY RUSHABH GALA
+# rushabhvg
+# GitHub :
+# https://github.com/rushabhvg
+# StackOverflow :
+# https://stackoverflow.com/users/16571212/rushabhvg
+# LinkedIn:
+# https://www.linkedin.com/in/rushabhvg/
+# */
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'biggerIsGreater' function below.
+#
+# The function is expected to return a STRING.
+# The function accepts STRING w as parameter.
+#
+
+def biggerIsGreater(w):
+    for i in range(1, len(w)):
+        if(w[-i-1]<w[-i]):
+            head = w[:-i-1]
+            tail = sorted(w[-i-1:])
+            nxt = min([a for a in tail if a>w[-i-1]])
+            tail.remove(nxt)
+            return head+nxt+''.join(tail)
+    return 'no answer'
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    T = int(input().strip())
+
+    for T_itr in range(T):
+        w = input()
+
+        result = biggerIsGreater(w)
+
+        fptr.write(result + '\n')
+
+    fptr.close()
+
+# /*
+# BY RUSHABH GALA
+# rushabhvg
+# GitHub :
+# https://github.com/rushabhvg
+# StackOverflow :
+# https://stackoverflow.com/users/16571212/rushabhvg
+# LinkedIn:
+# https://www.linkedin.com/in/rushabhvg/
+# */
