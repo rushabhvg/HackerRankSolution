145425a8a00c8134e093613ab87bed24ed1ce50e
RUSHABH GALA <88271018+rushabhvg@users.noreply.github.com>
2023-08-11 23:58:19 +0530

    Create absolute_permutation.py

---
+++ Absolute Permutation/absolute_permutation.py
@@ -0,0 +1,80 @@
+# /*
+# BY RUSHABH GALA
+# rushabhvg
+# GitHub :
+# https://github.com/rushabhvg
+# StackOverflow :
+# https://stackoverflow.com/users/16571212/rushabhvg
+# LinkedIn:
+# https://www.linkedin.com/in/rushabhvg/
+# */
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'absolutePermutation' function below.
+#
+# The function is expected to return an INTEGER_ARRAY.
+# The function accepts following parameters:
+#  1. INTEGER n
+#  2. INTEGER k
+#
+
+def absolutePermutation(n, k):
+    # Write your code here
+    if(k==0):
+        a = []
+        for i in range(0, n):
+            a.append(i+1)
+        return a
+    if ( (n/k)%2!=0):
+        return [-1]
+    add = True
+    perm = []
+    for i in range(1, n+1):
+        if add:
+            perm.append(i+k)
+        else:
+            perm.append(i-k)
+        if i % k == 0:
+            if add:
+                add = False
+            else:
+                add = True
+    return perm
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    t = int(input().strip())
+
+    for t_itr in range(t):
+        first_multiple_input = input().rstrip().split()
+
+        n = int(first_multiple_input[0])
+
+        k = int(first_multiple_input[1])
+
+        result = absolutePermutation(n, k)
+
+        fptr.write(' '.join(map(str, result)))
+        fptr.write('\n')
+
+    fptr.close()
+
+# /*
+# BY RUSHABH GALA
+# rushabhvg
+# GitHub :
+# https://github.com/rushabhvg
+# StackOverflow :
+# https://stackoverflow.com/users/16571212/rushabhvg
+# LinkedIn:
+# https://www.linkedin.com/in/rushabhvg/
+# */
